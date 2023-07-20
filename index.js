2c27f69a5b9ab19d4d060fb73ad6fa0f73fff95a
Gabriel Cristiano Ferraez <134634881+GabrielFerrarez19@users.noreply.github.com>
2023-07-20 09:00:46 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -15,73 +15,59 @@ closeBtn.addEventListener('click', () => {
 
 
 
-Orders.forEach(order => {
-    const tr = document.createElement('tr');
-    const trContent = `
-        <td>${order.productName}</td>
-        <td>${order.productNumber}</td>
-        <td>${order.paymentStatus}</td>
-        <td class="${order.status === 'Declined' ? 'danger' : order.status === 'Pending' ? 'warning' : 'primary'}">${order.status}</td>
-        <td class="primary">Details</td>
-    `;
-    tr.innerHTML = trContent;
-    document.querySelector('table tbody').appendChild(tr);
-});
+
 
 
 
 
  //script grafico
- const data = [10, 30, 50, 20, 40, 70, 80, 25, 45, 60, 15, 35];
- const months = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
+ document.addEventListener('DOMContentLoaded', function () {
+  // Função para obter os últimos 6 meses
+  function getLast6Months() {
+    const months = [];
+    const currentDate = new Date();
+    for (let i = 5; i >= 0; i--) {
+      const month = currentDate.getMonth() - i;
+      const year = currentDate.getFullYear();
+      months.push(`${month + 1}/${year}`);
+    }
+    return months;
+  }
 
- const canvas = document.getElementById('barChart');
- const ctx = canvas.getContext('2d');
+  // Dados dos últimos 6 meses (substitua com seus próprios dados)
+  const data = [100, 150, 200, 180, 250, 300];
 
- // Configurações do gráfico
- const barWidth = 40;
- const barSpacing = 10;
- const chartMargin = 30;
- const chartHeight = canvas.height - 2 * chartMargin;
- const chartWidth = canvas.width - 2 * chartMargin;
+  // Meses dos últimos 6 meses (obtidos automaticamente)
+  const months = getLast6Months();
 
- //barras
- function drawBarChart(data) {
-   const maxValue = Math.max(...data);
-   const scale = chartHeight / maxValue;
+  // Crie o contexto do gráfico
+  const ctx = document.getElementById('lineChart').getContext('2d');
 
-   for (let i = 0; i < data.length; i++) {
-     const barHeight = data[i] * scale;
-     const x = chartMargin + i * (barWidth + barSpacing);
-     const y = canvas.height - chartMargin - barHeight;
-
-     // Gradiente barras 
-     const gradient = ctx.createLinearGradient(x, y, x, canvas.height - chartMargin);
-     gradient.addColorStop(0, 'rgb(5, 68, 104)'); // Cor inicial do gradiente 
-     gradient.addColorStop(1, 'rgba(0, 123, 255, 0.2)'); // Cor final do gradiente 
-
-     ctx.fillStyle = gradient;
-     ctx.fillRect(x, y, barWidth, barHeight);
-
-     // Escreve o valor das barras
-     ctx.fillStyle = '#000'; // Cor do texto 
-     ctx.font = '12px Arial';
-     ctx.fillText(data[i], x + barWidth / 2 - 8, y - 8);
-   }
- }
-
- // Função  meses em baixo das barras
- function drawMonths() {
-   ctx.fillStyle = '#000'; // Cor do texto 
-   ctx.font = '12px Arial';
-
-   for (let i = 0; i < months.length; i++) {
-     const x = chartMargin + i * (barWidth + barSpacing) + barWidth / 2;
-     const y = canvas.height - chartMargin + 15;
-
-     ctx.fillText(months[i], x - 12, y);
-   }
- }
-
- drawBarChart(data);
- drawMonths();
+  // Crie o gráfico de linhas
+  const lineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: months, // Use os meses como rótulos do eixo X
+      datasets: [{
+        label: '',
+        borderColor: 'rgb(5, 68, 104)   ',
+        backgroundColor: 'rgba(0, 0, 255, 0.2)',
+        data: data, // Use os dados dos últimos 6 meses
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        x: {
+          grid: {
+            display: false // Ocultar linhas de grade no eixo X
+          }
+        },
+        y: {
+          beginAtZero: true // Começar o eixo Y a partir do valor zero
+        }
+      }
+    }
+  });
+});
